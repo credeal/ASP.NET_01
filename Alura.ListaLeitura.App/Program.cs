9aabf1de85511ce8ab5ef7f0fc629ba6c13b00ea
Fulano <detias@outlook.com>
2021-02-16 19:20:42 -0300

    Adicionado o .net Core e criado a classe Startup

--- Alura.ListaLeitura.App/Program.cs
+++ Alura.ListaLeitura.App/Program.cs
@@ -1,18 +1,26 @@
 ﻿using Alura.ListaLeitura.App.Negocio;
 using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Hosting;
 using System;
 
 namespace Alura.ListaLeitura.App
 {
     class Program
-    {
+    { 
         static void Main(string[] args)
         {
             var _repo = new LivroRepositorioCSV();
 
-            ImprimeLista(_repo.ParaLer);
-            ImprimeLista(_repo.Lendo);
-            ImprimeLista(_repo.Lidos);
+            IWebHost host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+
+            //ImprimeLista(_repo.ParaLer);
+            //ImprimeLista(_repo.Lendo);
+            //ImprimeLista(_repo.Lidos);
         }
 
         static void ImprimeLista(ListaDeLeitura lista)
